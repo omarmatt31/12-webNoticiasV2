4af9be9bcce6007b84ca7b253f685d9d7e68ad97
Omar Mattos <omarmatt84@gmail.com>
2025-06-29 22:47:40 -0300

    se limpio la estructura del proyecto, se instaló las librerias de react bootstrap y reac hook form, y se puso el main y footer de la pagina principal

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+ 
+  return (
+    <>
+      <main>
+        <h1>Hola Mundo</h1>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        <p>💻 Omar Mattos 💻</p>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
